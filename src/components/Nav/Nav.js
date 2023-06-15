7ff087107bab413136cacca6ee009853cf82f11a
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-06-15 15:01:15 +1000

    styled Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,20 @@
 import { NavLink } from "react-router-dom"
+import Logo from "../../assets/images/logo.svg"
 import "./Nav.css"
 
 const Nav = () => {
     return (
-        <nav>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/portfolio">Portfolio</NavLink></li>
-                <li><NavLink to="/contact">Contact Me</NavLink></li>
-            </ul>
-        </nav>
+        <div className="navBar">
+            <img src = {Logo} alt="logo" className="logo"/>
+            <nav>
+                <ul>
+                    <li><NavLink to="/">HOME</NavLink></li>
+                    <li><NavLink to="/portfolio">PORTFOLIO</NavLink></li>
+                    <li><NavLink to="/contact">CONTACT ME</NavLink></li>
+                </ul>
+            </nav>
+        </div>
+
     )
 }
 

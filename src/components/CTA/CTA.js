cec31b38823c9557fb7df48551f4af679994b519
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-06-23 14:27:28 +1000

    styled CTA

--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+import "./CTA.css"
+
 const CTA = () => {
     return (
         <>
-            <div className="container">
-                <h1>CTA</h1>
+            <div className="ctaContainer">
+                <h2>Interested in doing a project together?</h2>
+                <div className="divLine"></div>
+                <Link to="/contact" className="contactMeLink"><div className="simpleLink">CONTACT ME</div></Link>
             </div>
         </>
     )

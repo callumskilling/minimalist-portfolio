601e4b497a34231ee73664d0ef564ae593e2b7ed
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-06-23 15:44:21 +1000

    styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom"
+import Logo from "../../assets/images/logo.svg"
+import GitHub from "../../assets/images/icons/github.svg"
+import Twitter from "../../assets/images/icons/twitter.svg"
+import LinkedIn from "../../assets/images/icons/linkedin.svg"
+import "./Footer.css"
+
 const Footer = () => {
     return (
         <>
-            <div className="container">
-                <h1>Footer</h1>
+            <div className="footerContainer">
+                <div className="footerLogo">
+                <img src={Logo} alt="logo" className="logo" />
+                </div>
+                <div className="footerNav">
+                    <Link to="">HOME</Link>
+                    <Link to="/portfolio">PORTFOLIO</Link>
+                    <Link to="/contact">CONTACT ME</Link>
+                </div>
+                <div className="socialMediaIcons">
+                    <img src={GitHub} alt="Github Logo" className="socialMediaIcon" />
+                    <img src={Twitter} alt="Twitter Logo" className="socialMediaIcon" />
+                    <img src={LinkedIn} alt="LinkedIn Logo" className="socialMediaIcon" />
+                </div>
+
             </div>
         </>
     )

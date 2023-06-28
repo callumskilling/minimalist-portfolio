d6898e5a86c4ca05d54f0fccbb69175d627325d4
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-06-28 10:09:34 +1000

    added regex validation to email field

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,9 +1,12 @@
 export const Validation = (values) => {
     const errors = {}
+    const email_pattern = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
     if(values.name === "") {errors.name = "This field is required"}
-    if(values.message === "") {errors.message = "This field is required"}
-    if(values.email === "") {errors.email = "This field is required"}
 
+    if(values.message === "") {errors.message = "This field is required"}
+
+    if(values.email === "") {errors.email = "This field is required"} else if(!email_pattern.test(values.email)) {errors.email = "Please use a valid email address"}
+    
     return (errors)
 }
